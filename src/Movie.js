1514cefe445bc70e429397279998ce05db440d04
KIMINJOONG <ijboym216@gmail.com>
2018-11-28 00:31:50 +0900

    dumb component and smart component dumb : dont have 'state' smart: have 'state'

--- src/Movie.js
+++ src/Movie.js
@@ -3,32 +3,29 @@ import PropTypes from 'prop-types';
 import './Movie.css';
 
 
-class Movie extends Component {
-    
-    static propTypes = {
-        title : PropTypes.string
-        , poster : PropTypes.string
-    }
-    
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>    
-        )
-    }
+
+
+function Movie({title, poster}) {
+    return (
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
+}
+Movie.propTypes = {
+    title : PropTypes.string.isRequired
+    , poster : PropTypes.string.isRequired
 }
 
-class MoviePoster extends Component{
-    static propTypes = {
+
+function MoviePoster({poster}) {
+    return (
+        <img src={poster} />
+        )
+}
+    MoviePoster.propTypes = {
         poster : PropTypes.string.isRequired
     }
-    
-    render() {
-        return (
-            <img src={this.props.poster} />
-            )
-    }
-}
+
 export default Movie;
