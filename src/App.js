7445aceea908d2d1951d00f6f67edc3aa419e364
KIMINJOONG <ijboym216@gmail.com>
2018-11-29 22:03:41 +0900

    created new function

--- src/App.js
+++ src/App.js
@@ -12,15 +12,27 @@ class App extends Component {
     state = {}
 
     componentDidMount() {
-        fetch('https://yts.am/api/v2/list_movies.json?quality=3Dsort by=rating')
+        this._getMovies();
     }
 
 _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-        return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => {
+        return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies;
 }
+
+ _getMovies = async () => {
+        const movies = await this._callApi();
+        this.setState({
+            movies
+        })
+    }
+
+_callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?quality=3Dsort by=rating').then(response => response.json()).then(json => json.data.movies).catch(err => console.log(err))
+}
+                                         
     
   render() {
     return (
